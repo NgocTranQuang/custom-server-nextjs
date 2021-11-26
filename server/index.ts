954f14dfa508d4b485363986e3bafa15236b76a4
ngoctran <ngoctq110309@gmail.com>
2021-11-26 10:54:25 +0700

    a

--- server/index.ts
+++ server/index.ts
@@ -16,7 +16,7 @@ app.prepare().then(() => {
     handle(req, res, parsedUrl)
   }).listen(port)
 
-  // tslint:disable-next-line:no-console
+  // tslint:disable-next-line:no-console dfdfd
   console.log(
     `> Server listening at http://localhost:${port} as ${dev ? 'development' : process.env.NODE_ENV
     }`
